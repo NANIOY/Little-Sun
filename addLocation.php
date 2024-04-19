4b7d229ed546e4769b23511a2f760c187d6808e4
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-19 20:51:28 +0200

    refactor: change style to general form

--- addLocation.php
+++ addLocation.php
@@ -25,32 +25,32 @@ if (!empty($_POST)) {
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Add Location</title>
 	<link rel="stylesheet" href="css/global.css">
-	<link rel="stylesheet" href="css/location.css">
+	<link rel="stylesheet" href="css/form.css">
 </head>
 
 <body>
-	<div class="locationForm">
-		<h2 class="locationForm__title">Add Location</h2>
-		<form action="" method="post" class="locationForm__form">
-			<div class="locationForm__form__field">
-				<label for="location_name" class="locationForm__form__field__label">Location name:</label>
-				<input type="text" id="location_name" name="name" class="locationForm__form__field__input" required>
+	<div class="formContainer">
+		<h2 class="formContainer__title">Add Location</h2>
+		<form action="" method="post" class="formContainer__form">
+			<div class="formContainer__form__field">
+				<label for="location_name" class="formContainer__form__field__label">Location name:</label>
+				<input type="text" id="location_name" name="name" class="formContainer__form__field__input" required>
 			</div>
-			<div class="locationForm__form__field">
-				<label for="location_address" class="locationForm__form__field__label">Address:</label>
-				<textarea id="location_address" name="address" class="locationForm__form__field__textarea"
+			<div class="formContainer__form__field">
+				<label for="location_address" class="formContainer__form__field__label">Address:</label>
+				<textarea id="location_address" name="address" class="formContainer__form__field__textarea"
 					required></textarea>
 			</div>
-			<div class="locationForm__form__field">
-				<label for="contact_info" class="locationForm__form__field__label">Contact info:</label>
-				<input type="text" id="contact_info" name="contact_info" class="locationForm__form__field__input"
+			<div class="formContainer__form__field">
+				<label for="contact_info" class="formContainer__form__field__label">Contact info:</label>
+				<input type="text" id="contact_info" name="contact_info" class="formContainer__form__field__input"
 					required>
 			</div>
-			<div class="locationForm__form__field">
-				<label for="manager_id" class="locationForm__form__field__label">Manager ID:</label>
-				<input type="text" id="manager_id" name="manager_id" class="locationForm__form__field__input" required>
+			<div class="formContainer__form__field">
+				<label for="manager_id" class="formContainer__form__field__label">Manager ID:</label>
+				<input type="text" id="manager_id" name="manager_id" class="formContainer__form__field__input" required>
 			</div>
-			<button type="submit" class="locationForm__form__button">Add location</button>
+			<button type="submit" class="formContainer__form__button">Add location</button>
 		</form>
 	</div>
 </body>
