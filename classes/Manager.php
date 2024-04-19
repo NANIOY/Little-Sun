15b8d89e9e86e6b599b75e2b7fb943dd4f068d08
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-19 20:47:36 +0200

    feat: add getters and setters

---
+++ classes/Manager.php
@@ -0,0 +1,154 @@
+<?php
+
+
+class Manager
+{
+    private $firstName;
+    private $lastName;
+    private $email;
+    private $password;
+    private $profileImg;
+    private $hubLocation;
+    private $role = 'manager';
+
+
+    /**
+     * Get the value of firstName
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set the value of firstName
+     *
+     * @return  self
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of lastName
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set the value of lastName
+     *
+     * @return  self
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of profileImg
+     */
+    public function getProfileImg()
+    {
+        return $this->profileImg;
+    }
+
+    /**
+     * Set the value of profileImg
+     *
+     * @return  self
+     */
+    public function setProfileImg($profileImg)
+    {
+        $this->profileImg = $profileImg;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of hubLocation
+     */
+    public function getHubLocation()
+    {
+        return $this->hubLocation;
+    }
+
+    /**
+     * Set the value of hubLocation
+     *
+     * @return  self
+     */
+    public function setHubLocation($hubLocation)
+    {
+        $this->hubLocation = $hubLocation;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of role
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * Set the value of role
+     *
+     * @return  self
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+}
