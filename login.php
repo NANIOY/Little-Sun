592f1f1d96a8f0e5e87d3e10397508306ed6378d
MiniJJackson <116001889+MiniJJackson@users.noreply.github.com>
2024-04-19 11:28:15 +0200

    feat: added new file login.php - frontend

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LITTLESUN☀️</title>
+</head>
+<body>
+    <div id="navbar">
+        <div id="logo">LITTLESUN☀️</div>
+        <ul><a href="#">Item 1</a></ul>
+        <ul><a href="#">Item 2</a></ul>
+        <ul><a href="#">Item 3</a></ul>
+        <button>Button?</button>
+    </div>
+</body>
+</html>
