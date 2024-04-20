270f5b6b014be51d9f673dbd753b7fe1b5f1db23
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-20 16:00:48 +0200

    feat: add header + button

--- managers.php
+++ managers.php
@@ -15,24 +15,30 @@ $managers = Manager::getAll();
 </head>
 
 <body>
-    <div class="managercards">
-        <?php foreach ($managers as $manager): ?>
-            <div class="managercard">
-                <img src="<?php echo $manager['profile_img']; ?>" alt="Profile Image" class="managercard__img">
-                <div class="managercard__info">
-                    <div class="managercard__name">
-                        <?php echo $manager['first_name'] . ' ' . $manager['last_name']; ?>
-                    </div>
-                    <?php if (isset($manager['location_name'])): ?>
-                        <div class="managercard__hub">
-                            Hub Location: <?php echo $manager['location_name']; ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-            </div>
-        <?php endforeach; ?>
-    </div>
+    <div class="managers">
+        <div class="managers__header">
+            <h2>All Managers</h2>
+            <a href="addManager.php" class="managers__header__button">Add manager</a>
+        </div>
 
+        <div class="managercards">
+            <?php foreach ($managers as $manager): ?>
+                <div class="managercard">
+                    <img src="<?php echo $manager['profile_img']; ?>" alt="Profile Image" class="managercard__img">
+                    <div class="managercard__info">
+                        <div class="managercard__name">
+                            <?php echo $manager['first_name'] . ' ' . $manager['last_name']; ?>
+                        </div>
+                        <?php if (isset($manager['location_name'])): ?>
+                            <div class="managercard__hub">
+                                Hub Location: <?php echo $manager['location_name']; ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </body>
 
 </html>
