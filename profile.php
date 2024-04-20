e66c81d95b8b9fdeca9002b8919eb347bf1d23e2
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-20 16:25:37 +0200

    style: style profile a bit

--- profile.php
+++ profile.php
@@ -15,22 +15,30 @@ if (isset($_GET['id'])) {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Manager Profile</title>
             <link rel="stylesheet" href="css/global.css">
+            <link rel="stylesheet" href="css/profile.css">
         </head>
 
         <body>
-            <div class="manager-profile">
-                <h2><?php echo $manager['first_name'] . ' ' . $manager['last_name']; ?></h2>
-                <p>Email: <?php echo $manager['email']; ?></p>
-                <?php if (isset($manager['location_name'])): ?>
-                    <p>Hub Location: <?php echo $manager['location_name']; ?></p>
-                <?php else: ?>
-                    <p>Hub Location: Not specified</p>
-                <?php endif; ?>
-                <img src="<?php echo $manager['profile_img']; ?>" alt="Profile Image">
+            <div class="container">
+                <div class="manager-profile">
+                    <div class="profile-header">
+                        <img src="<?php echo $manager['profile_img']; ?>" alt="Profile Image" class="profile-img">
+                        <div class="profile-info">
+                            <h2 class="profile-name"><?php echo $manager['first_name'] . ' ' . $manager['last_name']; ?></h2>
+                            <p class="profile-email">Email: <?php echo $manager['email']; ?></p>
+                            <?php if (isset($manager['location_name'])): ?>
+                                <p class="profile-location">Hub Location: <?php echo $manager['location_name']; ?></p>
+                            <?php else: ?>
+                                <p class="profile-location">Hub Location: Not specified</p>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
             </div>
         </body>
 
         </html>
+
         <?php
     } else {
         echo 'Manager not found.';
