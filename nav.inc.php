5498ac4d05928b2dd3151e9288c3168029ef3d55
MiniJJackson <116001889+MiniJJackson@users.noreply.github.com>
2024-04-20 23:50:57 +0200

    feat: added nav.inc.php - clean code

---
+++ nav.inc.php
@@ -0,0 +1,6 @@
+<div id="navbar">
+            <div id="logo"><img src="img\Little-Sun-Logo-@2x.png" alt="LittleSunLogo"></div>
+            <ul><a href="#">Feature 1</a></ul>
+            <ul><a href="#">Feature 2</a></ul>
+            <ul><a href="#">Help</a></ul>
+</div>
