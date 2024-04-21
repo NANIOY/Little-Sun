37efc706d5675f2d7bc70a397f1428f0d4791b05
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-21 13:06:40 +0200

    style: navbar

--- includes/adminNav.inc.php
+++ includes/adminNav.inc.php
@@ -1,10 +1,9 @@
 <nav class="sidenav">
-    <a href="index.php" class="logo">Little Sun</a>
-    <a href="dashboard.php">Dashboard</a>
-    <a href="managers.php">Managers</a>
-    <a href="hublocations.php">Hub Locations</a>
-
-    <a href="logout.php" class="navbar__logout">Logout</a>
+    <a href="index.php" class="logo text-white"><img src="src\img\Little-Sun-Logo-@2x.png" alt="LittleSunLogo"></a>
+    <a href="dashboard.php" class="text-white"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+    <a href="managers.php" class="text-white"><i class="fas fa-users"></i> Managers</a>
+    <a href="hublocations.php" class="text-white"><i class="fas fa-map-marker-alt"></i> Hub Locations</a>
+    <a href="logout.php" class="navbar__logout text-white"><i class="fas fa-sign-out-alt"></i> Logout</a>
 </nav>
 
 <style>
@@ -14,29 +13,36 @@
         position: fixed;
         top: 0;
         left: 0;
-        background-color: #333;
         padding-top: 20px;
+        background-color: var(--black);
+    }
+
+    .logo img {
+        width: 100%;
     }
 
     .sidenav a {
         padding: 16px;
         text-decoration: none;
         font-size: 18px;
-        color: #fff;
-        display: block;
+        display: flex;
+        transition: 0.3s;
     }
 
     .sidenav a:hover {
-        background-color: #555;
+        color: var(--blurple);
+    }
+
+    .sidenav a i {
+        margin-right: 10px;
     }
 
     .navbar__logout {
-        color: #fff;
         display: block;
         box-sizing: border-box;
         position: absolute;
         bottom: 20px;
         width: 100%;
-        border-top: 1px solid #555;
+        border-top: 1px solid var(--gray);
     }
 </style>
