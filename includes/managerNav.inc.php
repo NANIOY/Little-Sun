ef8291aaa24ae72d669f77ac905ea0b747a72e29
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-21 17:12:17 +0200

    fix: dashboard link

--- includes/managerNav.inc.php
+++ includes/managerNav.inc.php
@@ -1,7 +1,7 @@
 <nav class="sidenav">
     <div class="sidenav__top">
         <a href="index.php" class="logo text-white"><img src="src\img\Little-Sun-Logo-@2x.png" alt="LittleSunLogo"></a>
-        <a href="dashboard.php" class="text-white"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+        <a href="managerDashboard.php" class="text-white"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
         <a href="#" class="text-white"><i class="fas fa-users"></i> Workers</a>
         <a href="#" class="text-white"><i class="fas fa-tasks"></i> Tasks</a>
         <a href="#" class="text-white"><i class="far fa-calendar"></i> Schedule</a>
