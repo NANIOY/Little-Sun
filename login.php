29224f722a97f4f713e1e5624066d44ef4a568a9
MiniJJackson <116001889+MiniJJackson@users.noreply.github.com>
2024-04-21 18:18:46 +0200

    refactor: change title

--- login.php
+++ login.php
@@ -36,7 +36,7 @@ if (!empty($_POST)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LITTLESUN☀️</title>
+    <title>LITTLESUN ☀️ | Login </title>
     <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="css/pagestyles/form.css">
     <link rel="stylesheet" href="css/pagestyles/login.css">
