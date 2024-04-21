6e7a676c0bfcc635f7718a976929ebc9156b8f07
MiniJJackson <116001889+MiniJJackson@users.noreply.github.com>
2024-04-21 18:25:07 +0200

    feat: add workers.php

---
+++ workers.php
@@ -0,0 +1,21 @@
+<?php
+
+
+?><!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Little Sun | Workers</title>
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/pagestyles/workers.css">
+</head>
+
+<?php include_once ("./includes/managerNav.inc.php"); ?>
+
+<body>
+    <div class="workers"></div>
+</body>
+
+</html>
