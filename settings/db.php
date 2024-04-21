8eafb41cb4afdee74d1db5acf3305d03da43a5b9
MiniJJackson <116001889+MiniJJackson@users.noreply.github.com>
2024-04-21 17:33:37 +0200

    Revert "refactor: changed nav to match"
    This reverts commit 67fd01f6bc328604d822123d084d4b630803c8e3.

--- settings/db.php
+++ settings/db.php
@@ -4,7 +4,7 @@ const SETTINGS = [
     'db' => [
         'user'      => 'root',
         'password'  => '',
-        'host'      => '127.0.0.1',
+        'host'      => 'localhost',
         'port'      => 3306,
         'database'  => 'littlesun',
     ],
