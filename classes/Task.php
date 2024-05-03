10ae9a7b36dd0f6ce1d0cee09952bdc527988282
Yannick Van den Brande <yavadebr@gmail.com>
2024-05-03 16:51:10 +0200

    feat: add task class

---
+++ classes/Task.php
@@ -0,0 +1,43 @@
+<?php
+include_once (__DIR__ . '/../bootstrap.php');
+
+class Task
+{
+    private $id;
+    private $title;
+    private $color;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function setColor($color)
+    {
+        $this->color = $color;
+        return $this;
+    }
+
+}
