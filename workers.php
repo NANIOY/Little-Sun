f9390ef54fe107bc0c6a4801e0d08dbc33d3b7fe
Yannick Van den Brande <yavadebr@gmail.com>
2024-05-04 17:37:58 +0200

    feat: display all workers in location

--- workers.php
+++ workers.php
@@ -7,6 +7,10 @@ requireManager();
 
 $workers = User::getAllWorkers();
 
+include_once (__DIR__ . '/classes/Manager.php');
+
+
+
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -17,29 +21,31 @@ $workers = User::getAllWorkers();
     <title>Little Sun | Workers</title>
     <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="css/pagestyles/workers.css">
+    <link rel="stylesheet" href="css/pagestyles/managers.css">
 </head>
 
 <?php include_once ("./includes/managerNav.inc.php"); ?>
 
 <body>
     <div class="workers">
-        
-    <div class="workers__header">
+        <div class="workers__header">
             <h3>All Workers</h3>
             <button onclick="window.location.href='addWorker.php'" class="button--primary">Add worker</button>
         </div>
+
         <div class="workercards">
-                <?php foreach ($workers as $workers): ?>
-                    <a href="profile.php?id=<?php echo $workers['id']; ?>" class="workercard">
-                        <img src="<?php echo $workers['profile_img']; ?>" alt="Profile Image" class="workercard__img profileimg">
-                        <div class="workercard__info">
-                            <div class="text-bold-normal">
-                                <?php echo $workers['first_name'] . ' ' . $workers['last_name']; ?>
-                            </div>
+            <?php foreach ($workers as $worker): ?>
+                <a href="profile.php?id=<?php echo $worker['id']; ?>" class="workercard">
+                    <img src="<?php echo $worker['profile_img']; ?>" alt="Profile Image"
+                        class="workercard__img profileimg">
+                    <div class="workercard__info">
+                        <div class="text-bold-normal">
+                            <?php echo $worker['first_name'] . ' ' . $worker['last_name']; ?>
                         </div>
-                    </a>
-                <?php endforeach; ?>
-            </div>
+                    </div>
+                </a>
+            <?php endforeach; ?>
+        </div>
     </div>
 </body>
 
