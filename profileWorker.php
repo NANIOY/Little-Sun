2729d3f057780262570a06a3fffa29b5248e46e1
Yannick Van den Brande <yavadebr@gmail.com>
2024-05-04 18:27:05 +0200

    feat: add workerProfile (WIP)

---
+++ profileWorker.php
@@ -0,0 +1,80 @@
+<?php
+include_once (__DIR__ . '/classes/User.php');
+include_once (__DIR__ . '/classes/Task.php');
+include_once (__DIR__ . '/includes/auth.inc.php');
+
+requireManager();
+
+if (isset($_GET['id'])) {
+    $workerId = $_GET['id'];
+    $worker = User::getById($workerId);
+
+    if ($worker) {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['task_ids'])) {
+            try {
+                $worker->assignTasks($_POST['task_ids']);
+                $message = "Tasks successfully updated.";
+            } catch (Exception $e) {
+                $message = "Error updating tasks: " . $e->getMessage();
+            }
+        }
+
+        $allTasks = Task::getAll();
+        $workerTasks = Task::getTasksByWorkerId($workerId);
+        $workerTaskIds = $workerTasks ? array_column($workerTasks, 'task_id') : [];
+
+        ?><!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Worker Profile</title>
+            <link rel="stylesheet" href="css/global.css">
+            <link rel="stylesheet" href="css/pagestyles/workerprofile.css">
+        </head>
+
+        <?php include_once ("./includes/managerNav.inc.php"); ?>
+
+        <body>
+            <div class="workerprofile">
+                <div class="workerprofile__details">
+                    <img src="<?php echo htmlspecialchars($worker['profile_img']); ?>" alt="Profile Image" class="profile__img profileimg">
+                    <div>
+                        <h3><?php echo htmlspecialchars($worker['first_name']) . ' ' . htmlspecialchars($worker['last_name']); ?></h3>
+                        <p class="text-reg-normal">Email: <?php echo htmlspecialchars($worker['email']); ?></p>
+                        <?php if (!empty($message)): ?>
+                        <p><?php echo $message; ?></p>
+                        <?php endif; ?>
+                        <button onclick="location.href='editWorkerProfile.php?id=<?php echo $workerId; ?>';" class="button--secondary">Edit Profile</button>
+                    </div>
+                </div>
+        
+                <div class="profile__tasks">
+                    <h4>Assign Tasks</h4>
+                    <form action="" method="post">
+                        <ul>
+                            <?php foreach ($allTasks as $task): ?>
+                            <li>
+                                <label class="profile__tasks__label">
+                                    <input type="checkbox" name="task_ids[]" value="<?php echo $task['id']; ?>"
+                                        <?php echo in_array($task['id'], $workerTaskIds) ? 'checked' : ''; ?>>
+                                    <?php echo htmlspecialchars($task['title']); ?>
+                                </label>
+                            </li>
+                            <?php endforeach; ?>
+                        </ul>
+                        <button type="submit" class="button--primary">Assign Tasks</button>
+                    </form>
+                </div>
+            </div>
+        </body>
+        </html>
+        <?php
+    } else {
+        echo 'Worker not found.';
+    }
+} else {
+    echo 'Worker ID not provided.';
+}
+?>
