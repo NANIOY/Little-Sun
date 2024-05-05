266d0f90534c30814c453ee87ea95e4e709275e6
Yannick Van den Brande <yavadebr@gmail.com>
2024-05-05 12:10:20 +0200

    feat: display tags

--- profileWorker.php
+++ profileWorker.php
@@ -22,7 +22,6 @@ if (isset($_GET['id'])) {
         if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['task_ids'])) {
             try {
                 $worker->assignTasks($_POST['task_ids']);
-                $message = "Tasks successfully updated.";
             } catch (Exception $e) {
                 $message = "Error updating tasks: " . $e->getMessage();
             }
@@ -40,6 +39,7 @@ if (isset($_GET['id'])) {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Worker Profile</title>
             <link rel="stylesheet" href="css/global.css">
+            <link rel="stylesheet" href="css/pagestyles/tasks.css">
             <link rel="stylesheet" href="css/pagestyles/workerprofile.css">
         </head>
 
@@ -48,29 +48,36 @@ if (isset($_GET['id'])) {
         <body>
             <div class="workerprofile">
                 <div class="workerprofile__details">
-                    <img src="<?php echo htmlspecialchars($workerData['profile_img']); ?>" alt="Profile Image" class="profile__img profileimg">
+                    <img src="<?php echo htmlspecialchars($workerData['profile_img']); ?>" alt="Profile Image"
+                        class="profile__img profileimg">
                     <div>
-                        <h3><?php echo htmlspecialchars($workerData['first_name']) . ' ' . htmlspecialchars($workerData['last_name']); ?></h3>
+                        <h3><?php echo htmlspecialchars($workerData['first_name']) . ' ' . htmlspecialchars($workerData['last_name']); ?>
+                        </h3>
                         <p class="text-reg-normal">Email: <?php echo htmlspecialchars($workerData['email']); ?></p>
-                        <?php if (!empty($message)): ?>
-                        <p><?php echo $message; ?></p>
-                        <?php endif; ?>
-                        <button onclick="location.href='editWorkerProfile.php?id=<?php echo $workerId; ?>';" class="button--secondary">Edit Profile</button>
+                        <div class="tasklist">
+                            <?php foreach ($workerTasks as $task): ?>
+                                <div class="tasklist__tag text-bold-normal"
+                                    style="background-color: <?php echo htmlspecialchars($task['color']); ?>">
+                                    <?php echo htmlspecialchars($task['title']); ?>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                        <button onclick="location.href='editWorkerProfile.php?id=<?php echo $workerId; ?>';"
+                            class="button--secondary">Edit Profile</button>
                     </div>
                 </div>
-        
+
                 <div class="profile__tasks">
                     <h4>Assign Tasks</h4>
                     <form action="" method="post">
                         <ul>
                             <?php foreach ($allTasks as $task): ?>
-                            <li>
-                                <label class="profile__tasks__label">
-                                    <input type="checkbox" name="task_ids[]" value="<?php echo $task['id']; ?>"
-                                        <?php echo in_array($task['id'], $workerTaskIds) ? 'checked' : ''; ?>>
-                                    <?php echo htmlspecialchars($task['title']); ?>
-                                </label>
-                            </li>
+                                <li>
+                                    <label class="profile__tasks__label">
+                                        <input type="checkbox" name="task_ids[]" value="<?php echo $task['id']; ?>" <?php echo in_array($task['id'], $workerTaskIds) ? 'checked' : ''; ?>>
+                                        <?php echo htmlspecialchars($task['title']); ?>
+                                    </label>
+                                </li>
                             <?php endforeach; ?>
                         </ul>
                         <button type="submit" class="button--primary">Assign Tasks</button>
@@ -78,6 +85,7 @@ if (isset($_GET['id'])) {
                 </div>
             </div>
         </body>
+
         </html>
         <?php
     } else {
