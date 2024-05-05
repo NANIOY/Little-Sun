d589f0d372e6efd62fab6a145921cb2aceec2c1e
MiniJJackson <116001889+MiniJJackson@users.noreply.github.com>
2024-05-05 20:52:44 +0200

    change nav redirect

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     session_start();
     if(isset($_SESSION['user'])){
         //echo 'Welcome ' . $_SESSION['user']['first_name'];
+    
+
     }else{
         header('Location: login.php');
     }
@@ -14,26 +16,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LITTLESUN☀️</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="https://use.typekit.net/qgt5qis.css">
 </head>
 <body>
     <header>
-        <div id="navbar">
-            <div id="logo"><img src="src\img\Little-Sun-Logo-@2x.png" alt="LittleSunLogo"></div>
-            <a href="#">Feature 1</a>
-            <a href="#">Feature 2</a>
-            <a href="profile.php" class="LoggedIn">
-                <?php if(isset($_SESSION['user']['first_name'])): ?>
-                <h3><?php echo $_SESSION['user']['first_name']; ?></h3>
-                <?php else: ?> 
-                <h3>User name here</h3>
-                <?php endif; ?>
-                <div class="user-avatar"><img src="https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_640.png" width="50px" alt="user avatar"></div>
-                
-            </a>
-            <a href="logout.php">Log out?</a>
-        </div>
+        <?php include_once ("./includes/nav.inc.php"); ?>
+
     </header>
     <main>
 
