38309c915eb6d758551f601047c85f82a5b08cd6
Yannick Van den Brande <yavadebr@gmail.com>
2024-05-12 16:42:36 +0200

    style: adjust calendar look

--- managerSchedule.php
+++ managerSchedule.php
@@ -1,42 +1,80 @@
 <?php
-include_once (__DIR__ . '/includes/auth.inc.php');
-include_once (__DIR__ . '/classes/TimeOff.php');
-include_once (__DIR__ . '/classes/User.php');
-include_once (__DIR__ . '/classes/Calender.php');
+function generateDaysForMonth($year, $month)
+{
+    $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $month, $year);
 
-requireManager();
+    $firstDayOfMonth = date('N', strtotime("$year-$month-01"));
 
+    $days = [];
 
-?><!DOCTYPE html>
+    for ($i = 1; $i < $firstDayOfMonth; $i++) {
+        $days[] = '';
+    }
+
+    for ($day = 1; $day <= $daysInMonth; $day++) {
+        $days[] = sprintf('%04d-%02d-%02d', $year, $month, $day);
+    }
+
+    return $days;
+}
+
+$currentYear = isset($_GET['year']) ? $_GET['year'] : date('Y');
+$currentMonth = isset($_GET['month']) ? $_GET['month'] : date('m');
+
+$allDaysThisMonth = generateDaysForMonth($currentYear, $currentMonth);
+?>
+
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Little Sun | Schedule</title>
+    <title>Calendar</title>
     <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="css/pagestyles/workerschedule.css">
     <link rel="stylesheet" href="css/pagestyles/calendar.css">
-
 </head>
 
 <body>
-    <?php include_once ("./includes/managerNav.inc.php"); ?>
+    <div class="container">
+        <?php include_once ("./includes/managerNav.inc.php"); ?>
 
-    <div class="workers">
-        <div class="workers__header">
-            <h3>Schedule</h3>
+        <div class="workers">
+            <div class="workers__header">
+                <h3>Schedule</h3>
+            </div>
+            <div class="calendar__navigation">
+                <button class="button--primary"
+                    onclick="navigateMonth(<?php echo ($currentMonth == 1) ? $currentYear - 1 : $currentYear; ?>, <?php echo ($currentMonth == 1) ? 12 : $currentMonth - 1; ?>)">Prev</button>
+                <h5><?php echo date('F Y', strtotime($currentYear . '-' . $currentMonth . '-01')); ?></h5>
+                <button class="button--primary"
+                    onclick="navigateMonth(<?php echo ($currentMonth == 12) ? $currentYear + 1 : $currentYear; ?>, <?php echo ($currentMonth == 12) ? 1 : $currentMonth + 1; ?>)">Next</button>
+            </div>
+            <div class="calendar">
+                <div>Mon</div>
+                <div>Tue</div>
+                <div>Wed</div>
+                <div>Thu</div>
+                <div>Fri</div>
+                <div>Sat</div>
+                <div>Sun</div>
+                <?php foreach ($allDaysThisMonth as $day): ?>
+                    <div class="day" onclick="navigateToAssignment('<?php echo $day; ?>')">
+                        <?php echo date('d', strtotime($day)); ?></div>
+                <?php endforeach; ?>
+            </div>
         </div>
 
-        <div class="workers__calendar">
-                <?php
-                    $calendar = new Calendar();
+        <script>
+            function navigateToAssignment(date) {
+                window.location.href = 'managerAssign.php?date=' + date;
+            }
 
-                    echo $calendar->show();
-                ?>
-           
-        </div>
-
+            function navigateMonth(year, month) {
+                window.location.href = '?year=' + year + '&month=' + month;
+            }
+        </script>
     </div>
 </body>
 
