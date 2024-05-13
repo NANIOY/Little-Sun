5168f80687963a03e9af2a5b391778764a6157d5
MiniJJackson <116001889+MiniJJackson@users.noreply.github.com>
2024-05-13 22:43:15 +0200

    feat: added worker assign sick page for form

---
+++ workerAssignSick.php
@@ -0,0 +1,14 @@
+<?php
+        echo "ASSIGN SICK FORM";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assign sick on day</title>
+</head>
+<body>
+    
+</body>
+</html>
