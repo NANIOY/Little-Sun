c4bd8e57f68426584561e603bfe94e7b5593c720
Yannick Van den Brande <yavadebr@gmail.com>
2024-05-18 23:14:47 +0200

    test: readd user content and remove db content

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,2 @@
 <?php
 
-include_once(__DIR__ . '/classes/Db.php');
-
-$db = Db::getInstance();
--- classes/Db.php
+++ classes/Db.php
@@ -6,29 +6,6 @@ abstract class Db
 
     public static function getInstance()
     {
-        require __DIR__ . '/../settings/db.php';
-
-        if (self::$conn === null) {
-            $db = $settings['db'];
-
-            $dsn = 'mysql:host=' . $db['host'] . ';port=' . $db['port'] . ';dbname=' . $db['database'] . ';charset=utf8mb4';
-
-            $options = [
-                PDO::MYSQL_ATTR_SSL_CA => $db['ssl']['ca'],
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES => false,
-                PDO::ATTR_TIMEOUT => 10,
-            ];
-
-            try {
-                self::$conn = new PDO($dsn, $db['user'], $db['password'], $options);
-            } catch (PDOException $e) {
-                die("Connection failed: " . $e->getMessage());
-            }
-        }
-
-        return self::$conn;
+        
     }
 }
