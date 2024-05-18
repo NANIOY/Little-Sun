a57115ae4d45e21f25c7f3e5c47d5ebc37803304
Yannick Van den Brande <yavadebr@gmail.com>
2024-05-18 18:49:12 +0200

    feat: connect

--- classes/Db.php
+++ classes/Db.php
@@ -9,10 +9,25 @@ abstract class Db
         include_once __DIR__ . '/../settings/db.php';
 
         if (self::$conn === null) {
-            self::$conn = new PDO('mysql:host=' . SETTINGS['db']['host'] . ';dbname=' . SETTINGS['db']['database'], SETTINGS['db']['user'], SETTINGS['db']['password']);
-            return self::$conn;
-        } else {
-            return self::$conn;
+            $db = SETTINGS['db'];
+
+            $dsn = 'mysql:host=' . $db['host'] . ';port=' . $db['port'] . ';dbname=' . $db['database'] . ';charset=utf8mb4';
+
+            $options = [
+                PDO::MYSQL_ATTR_SSL_CA => $db['ssl']['ca'],
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false,
+            ];
+
+            try {
+                self::$conn = new PDO($dsn, $db['user'], $db['password'], $options);
+            } catch (PDOException $e) {
+                die("Connection failed: " . $e->getMessage());
+            }
         }
+
+        return self::$conn;
     }
 }
