eeabca60e0394b458909fbe5d94775f5cb914bb2
Yannick Van den Brande <yavadebr@gmail.com>
2024-05-18 23:58:01 +0200

    test: add session_start

--- login.php
+++ login.php
@@ -1,8 +1,9 @@
 <?php
 session_save_path(__DIR__ . '/sessions');
+session_start();
 include_once (__DIR__ . '/classes/User.php');
 
-echo 'test session';
+echo 'test session 2';
 
 $users = User::getAll();
 
@@ -35,7 +36,6 @@ if (!empty($_POST)) {
         $error = true;
     }
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -51,20 +51,16 @@ if (!empty($_POST)) {
 
 <body>
     <main>
-
         <div class="LittleSunTitleShiftplanner">
             <h2>Little <strong style="color:#F7BD01;">Sun</strong> Shiftplanner</h2>
             <p>Welcome to Little Sun Shiftplanner, the ultimate platform for shift planners in Zambia! At Little Sun
                 Shiftplanner, we empower workers to take control of their schedules by defining their roles and
                 selecting preferred work locations. Our user-friendly interface allows workers to plan their
-                availability
-                for shifts and even schedule well-deserved vacations with ease.</p>
+                availability for shifts and even schedule well-deserved vacations with ease.</p>
         </div>
-
         <div class="formContainer">
             <h4 class="formContainer__title">Welcome</h4>
             <form action="" method="post" enctype="multipart/form-data" class="formContainer__form">
-
                 <?php if (isset($error)): ?>
                     <div class="form__error">
                         <p>
@@ -72,30 +68,23 @@ if (!empty($_POST)) {
                         </p>
                     </div>
                 <?php endif; ?>
-
                 <div class="formContainer__form__field">
                     <label for="email" class="text-reg-s">Email:</label>
                     <input type="email" id="email" name="email"
                         class="formContainer__form__field__input text-reg-normal" required>
                 </div>
-
                 <div class="formContainer__form__field">
                     <label for="password" class="text-reg-s">Password:</label>
                     <input type="password" id="password" name="password"
                         class="formContainer__form__field__input text-reg-normal" required>
                 </div>
-
                 <div class="formContainer__form__field">
                     <a href="#">Forgot password?</a>
                 </div>
-
                 <button type="submit" class="formContainer__form__button button--primary">Log In</button>
             </form>
         </div>
-
-
     </main>
-
 </body>
 
 </html>
