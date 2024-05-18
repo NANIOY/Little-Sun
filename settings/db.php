9108e5a7a606ee3c8333a7615ed77ec68660ed51
Yannick Van den Brande <yavadebr@gmail.com>
2024-05-18 21:52:07 +0200

    feat: env connection

--- settings/db.php
+++ settings/db.php
@@ -1,12 +1,18 @@
 <?php
 
-const SETTINGS = [
+require __DIR__ . '/env_loader.php';
+
+$settings = [
     'db' => [
-        'user'      => 'root',
-        'password'  => '',
-        'host'      => '127.0.0.1',
-        'port'      => 3306,
-        'database'  => 'littlesun',
+        'user' => getenv('DB_USER'),
+        'password' => getenv('DB_PASSWORD'),
+        'host' => getenv('DB_HOST'),
+        'port' => getenv('DB_PORT'),
+        'database' => getenv('DB_DATABASE'),
+        'ssl' => [
+            'key' => null,
+            'cert' => null,
+            'ca' => __DIR__ . '/BaltimoreCyberTrustRoot.crt.pem',
+        ],
     ],
 ];
-
