9819fa943deb5f6ce97ea47e80f13a71a2f8f6f9
Yannick Van den Brande <yavadebr@gmail.com>
2024-05-18 21:44:40 +0200

    feat: custom env loader

--- classes/Db.php
+++ classes/Db.php
@@ -6,10 +6,10 @@ abstract class Db
 
     public static function getInstance()
     {
-        include_once __DIR__ . '/../settings/db.php';
+        require __DIR__ . '/../settings/db.php';
 
         if (self::$conn === null) {
-            $db = SETTINGS['db'];
+            $db = $settings['db'];
 
             $dsn = 'mysql:host=' . $db['host'] . ';port=' . $db['port'] . ';dbname=' . $db['database'] . ';charset=utf8mb4';
 
@@ -19,6 +19,7 @@ abstract class Db
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_TIMEOUT => 10,
             ];
 
             try {
